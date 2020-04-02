b1e80c7ef488a3466c8186a79df6868e0979e512
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-02 23:29:56 +0800

    Add profile button

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -2,10 +2,14 @@ import React from "react";
 import Nav from "react-bootstrap/Nav"; // Single nav element
 import Navbar from "react-bootstrap/Navbar"; // The entire Navbar
 import Button from "react-bootstrap/Button";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Dropdown from "react-bootstrap/Dropdown";
+import { logoutUser } from "../../actions/authActions";
 
-function Header() {
+function Header(props) {
   return (
     <div>
       <header className="App-header">
@@ -20,11 +24,25 @@ function Header() {
 
           <Nav>
             <Nav.Item>
-              <button className="link-btn"><Link to={"/register"}>Register</Link></button>
+
+              {props.auth.isAuthenticated ? (
+                <Dropdown className="profile-dropdown">
+                  <Dropdown.Toggle><AccountCircleIcon style={{color: "#808080"}} fontSize="large"/><i id="icon" className="fa fa-arrow-down"></i></Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                    <Dropdown.Item as="button" onClick={props.logoutUser}>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : null}
+
             </Nav.Item>
 
             <Nav.Item>
-              <button className="link-btn"><Link to={"/login"}>Login</Link></button>
+              {!props.auth.isAuthenticated ? <button className="link-btn"><Link to={"/register"}>Register</Link></button> : null}
+            </Nav.Item>
+
+            <Nav.Item>
+              {!props.auth.isAuthenticated ? <button className="link-btn"><Link to={"/login"}>Login</Link></button> : null}
             </Nav.Item>
 
             <Nav.Item>
@@ -38,4 +56,14 @@ function Header() {
   )
 }
 
-export default Header;
+const mapStateToPros = state => ({
+  auth: state.auth
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logoutUser: () => dispatch(logoutUser())
+  }
+}
+
+export default connect(mapStateToPros, mapDispatchToProps)(Header);
