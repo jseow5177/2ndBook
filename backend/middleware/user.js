f18e1e88bb5b3c6842489cdb2173e64014db7aa4
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-05 15:48:06 +0800

    Add user middleware

---
+++ backend/middleware/user.js
@@ -0,0 +1,27 @@
+const jwt_decode = require("jwt-decode");
+const User = require("../models/User");
+
+async function getLoggedinUser(req, res, next) {
+
+    let loggedinUser;
+    const token = req.headers.authorization;
+    if (token) {
+        const decoded = jwt_decode(token);
+        const userId = decoded.id;
+        try {
+          loggedinUser = await User.findById(userId);
+          if (loggedinUser == null) {
+            return res.status(404).json({error: "User not found"}); // NEED TO RENDER 404
+          }
+        } catch(error) {
+          return res.status(500).json({error: error.message});
+        }
+        res.loggedinUser = loggedinUser;
+        next();
+    } else {
+        res.loggedinUser = null;
+        next();
+    }
+}
+
+module.exports = getLoggedinUser;
